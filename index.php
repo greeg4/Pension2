73697e34a683d795becb15654979b890e0b438aa
greeg4 <mathieu405@hotmail.com>
2015-10-26 13:44:12 +0100

    aaaaa
    1er_etape

---
+++ index.php
@@ -0,0 +1,29 @@
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Pension</title>
+
+    <link rel="stylesheet" href="./lib/css/p_style.css">
+
+
+</head>
+<body>
+    <div id="page">
+        <header class="img_header">			
+        </header>
+        <div class="row">
+            <div class="col-md-1"><button type="button" class="bAccueil">Accueil</button></div>            
+            <div class="col-md-1"><button type="button" class="bConfort">Notre confort</button></div>
+            <div class="col-md-1"><button type="button" class="bInfos">Infos pratiques</button></div>
+            <div class="col-md-1"><button type="button" class="bReservation">Réservations</button></div>
+            <div class="col-md-1"><button type="button" class="bGalerie">Galerie photos</button></div>
+        </div>
+    </div>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="lib/js/bootstrap.min.js"></script>
+</body>
+</html>
